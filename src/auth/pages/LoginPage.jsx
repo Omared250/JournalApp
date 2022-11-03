8c4ea15fa1922804254b8e7af42798dc379f8f0d
Omar <omared250@gmail.com>
2022-11-03 10:40:34 +0100

    login page

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,4 +1,6 @@
-import { Grid, TextField, Typography } from "@mui/material";
+import { Link as RouterLink } from 'react-router-dom';
+import { Button, Grid, Link, TextField, Typography } from "@mui/material";
+import { Google } from "@mui/icons-material";
 
 
 export const LoginPage = () => {
@@ -22,7 +24,7 @@ export const LoginPage = () => {
                     <Grid container>
                         <Grid item xs={ 12 } sx={{ mt: 2 }}>
                             <TextField 
-                                label="Correo" 
+                                label="Email" 
                                 type="email" 
                                 placeholder="correo@google.com"
                                 fullWidth 
@@ -36,6 +38,30 @@ export const LoginPage = () => {
                                 fullWidth 
                             />
                         </Grid>
+
+                        <Grid container spacing={ 2 } sx={{ mb: 2, mt: 1 }}>
+
+                            <Grid item xs={ 12 } sm={ 6 }>
+                                <Button variant='contained' fullWidth>
+                                    Login
+                                </Button>
+                            </Grid>
+
+                            <Grid item xs={ 12 } sm={ 6 }>
+                                <Button variant='contained' fullWidth>
+                                    <Google/>
+                                    <Typography sx={{ ml: 1 }}>Google</Typography>
+                                </Button>
+                            </Grid>
+
+                        </Grid>
+
+                        <Grid container direction='row' justifyContent='end'>
+                            <Link component={ RouterLink } color='inherit' to="/auth/register">
+                                Sign up
+                            </Link>
+                        </Grid>
+
                     </Grid>
                 </form>
 
