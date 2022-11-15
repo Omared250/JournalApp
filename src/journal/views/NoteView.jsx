def7ffaf8035888457dd9fda39204fd280e3a500
Omar <omared250@gmail.com>
2022-11-15 13:28:16 +0100

    Note viewfunctional

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,6 +1,40 @@
+import { SaveOutlined } from "@mui/icons-material"
+import { Button, Grid, TextField, Typography } from "@mui/material"
 
 export const NoteView = () => {
     return (
-        <div>NoteView</div>
+        <Grid container direction='row' justifyContent='space-between' alignItems='center' sx={{ mb: 1 }}>
+            <Grid item>
+                <Typography fontSize={ 39 } fontWeight='light'>28 de agosto, 2023</Typography>
+            </Grid>
+            <Grid item>
+                <Button color="primary" sx={{ padding: 2 }}>
+                    <SaveOutlined sx={{ fontSize: 30, mr: 1 }} />
+                    Save
+                </Button>
+            </Grid>
+            <Grid container>
+                <TextField
+                    type="text"
+                    variant="filled"
+                    fullWidth
+                    placeholder="Add new title"
+                    label="title"
+                    sx={{ border: 'none', mb: 1 }} 
+                />
+                <TextField
+                    type="text"
+                    variant="filled"
+                    fullWidth
+                    multiline
+                    placeholder="How was your day?"
+                    label=""
+                    minRows={ 5 }
+                />
+            </Grid>
+
+            {/* Image gallery */}
+            
+        </Grid>
     )
 }
