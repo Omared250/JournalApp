fca6f8f9c0a4ff5c3a0214b80b2f479343c74300
Omar <omared250@gmail.com>
2022-11-15 12:05:31 +0100

    None note selected view

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,17 +1,10 @@
-import { Typography } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
+import { NothingSelectedView } from '../views';
 
 export const JournalPage = () => {
     return (
         <JournalLayout>
-            <Typography>
-                Culpa veniam proident excepteur qui duis ad magna excepteur. 
-                Non sunt eu adipisicing eu laboris est fugiat sit ullamco quis. Dolore cupidatat ex 
-                duis proident ea commodo tempor veniam aliqua voluptate sit do ea mollit. Enim ut ad nostrud 
-                qui qui Lorem mollit esse nulla reprehenderit sunt. Proident aliqua aliqua nisi elit 
-                nisi et duis nulla magna. Nisi dolore est enim pariatur duis nulla proident quis reprehenderit 
-                et ullamco amet.
-            </Typography>
+            <NothingSelectedView />
         </JournalLayout>
     )
 }
