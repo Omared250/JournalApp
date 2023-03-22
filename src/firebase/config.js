6c590d4b86f476225a969947bb4e6fede0fe308b
Omared250 <79874354+Omared250@users.noreply.github.com>
2023-03-22 17:57:47 +0100

    adding firebase to the project

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -18,6 +18,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const firebaseApp = initializeApp(firebaseConfig);
-
 export const firebaseAuth = getAuth(firebaseApp);
 export const firebaseDB = getFirestore( firebaseDB );
