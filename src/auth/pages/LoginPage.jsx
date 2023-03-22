e78b82895ea86457b182c81cf0e86333429d11fb
Omared250 <79874354+Omared250@users.noreply.github.com>
2023-03-22 15:42:23 +0100

    sing in button and google working with fake data

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -2,19 +2,39 @@ import { Link as RouterLink } from 'react-router-dom';
 import { Button, Grid, Link, TextField, Typography } from "@mui/material";
 import { Google } from "@mui/icons-material";
 import { AuthLayout } from '../layout/AuthLayout';
+import { useForm } from '../../hooks';
 
 
 export const LoginPage = () => {
+
+    const { email, password, onInputChange } = useForm({
+        email: 'oascanioarias@google.com',
+        password: '123456',
+    });
+
+    const onSubmit = ( event ) => {
+        event.preventDefault();
+
+        console.log( { email, password } );
+    };
+
+    const onGoogleSignIn = () => {
+        console.log('onGoogleSignIn');
+    };
+
     return (
         <AuthLayout title="Login">
-            <form>
+            <form onSubmit={ onSubmit }>
                 <Grid container>
                     <Grid item xs={ 12 } sx={{ mt: 2 }}>
                         <TextField 
                             label="Email" 
                             type="email" 
                             placeholder="correo@google.com"
-                            fullWidth 
+                            fullWidth
+                            name="email"
+                            value={ email }
+                            onChange={ onInputChange } 
                         />
                     </Grid>
                     <Grid item xs={ 12 } sx={{ mt: 2 }}>
@@ -23,16 +43,19 @@ export const LoginPage = () => {
                             type="password" 
                             placeholder="password"
                             fullWidth 
+                            name="password"
+                            value={ password }
+                            onChange={ onInputChange }
                         />
                     </Grid>
                     <Grid container spacing={ 2 } sx={{ mb: 2, mt: 1 }}>
                         <Grid item xs={ 12 } sm={ 6 }>
-                            <Button variant='contained' fullWidth>
+                            <Button type="submit" variant='contained' fullWidth>
                                 Login
                             </Button>
                         </Grid>
                         <Grid item xs={ 12 } sm={ 6 }>
-                            <Button variant='contained' fullWidth>
+                            <Button onClick={ onGoogleSignIn } variant='contained' fullWidth>
                                 <Google/>
                                 <Typography sx={{ ml: 1 }}>Google</Typography>
                             </Button>
